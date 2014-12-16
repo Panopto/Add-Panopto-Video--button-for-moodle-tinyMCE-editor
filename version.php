89676d49d011d2389a99959425678f159e3eef24
Panopto, Inc. <jlin@panopto.com>
2014-12-16 10:48:44 -0800

    Changed Moodle version requirement
    Lowers required version of Moodle to install program from 2.7 to 2.0

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2014090501;  // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2014050800;  // Required Moodle version.
+$plugin->requires  = 2010112400;  // Required Moodle version.
 $plugin->component = 'tinymce_panoptobutton';  // Full name of the plugin (used for diagnostics).
 
 ?>
