5f6794a124f72f193bd394cf05c8eea68ccb49e4
Panopto, Inc. <Panopto@users.noreply.github.com>
2015-09-11 10:39:28 -0700

    Update version

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014090501;  // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015091100;  // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2010112400;  // Required Moodle version (translates to version 2.0).
 $plugin->component = 'tinymce_panoptobutton';  // Full name of the plugin (used for diagnostics).
 
