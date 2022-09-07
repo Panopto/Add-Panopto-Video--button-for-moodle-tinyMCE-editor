e751329bf0a531c9d53d5041fe558d9a5033e207
jmalmsten-panopto <jmalmsten@panopto.com>
2022-09-07 10:02:30 -0700

    2022090700 release version.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@ defined('MOODLE_INTERNAL') || die();
 
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version   = 2022070700;  
+$plugin->version   = 2022090700;  
 
 // Requires this Moodle version - 2.7.
 $plugin->requires  = 2014051200;  
